2d6e8304c452540f7d08cfd3faa39fa9840b0d68
John Comeau <jc@unternet.net>
2017-04-23 17:51:40 -0700

    slow progress

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var data = {"groups": []};
 
 app.post("/", function(req, res, next) {
   var body = "";
   req.on("data", function(chunk) {body += chunk.toString();});
-  req.on("end", function() {console.log("got POST: " + body);});
-  res.redirect('back');
+  req.on("end", function() {
+    console.log("got POST: " + body);
+    res.redirect("back");
+  });
 });
 
 app.use(express.static(__dirname + '/'));
