0b00f80356b2b1caba448c7e377c77a2bdcc6630
John Comeau <jc@unternet.net>
2017-04-25 18:36:16 -0700

    no longer needs express. must implement static files

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var data = {"groups": []};
-
-app.post("/", function(req, res, next) {
+var fs = require("fs");
+var app = function(request, response) {
   var body = "";
-  req.on("data", function(chunk) {body += chunk.toString();});
-  req.on("end", function() {
-    console.log("got POST: " + body);
-    res.redirect("back");
-  });
-});
-
-app.use(express.static(__dirname + '/'));
+  console.log("got " + request.method + " for " + request.url);
+  if (request.method == "GET") {
+    fs.readFile("index.html", function(error, data) {
+      if (error) return console.error(error);
+      response.write(data);
+      response.end();
+    });
+  } else if (request.method == "POST") {
+    request.on("data", function(chunk) {body += chunk.toString();});
+    req.on("end", function() {
+      console.log("got POST: " + body);
+      res.redirect("back");
+    });
+  } else {
+    console.error("unimplemented method: " + request.method);
+  }
+};
+var server = require("http").createServer(app);
+console.log("server: " + server);
+var io = require("socket.io").listen(server);
+var data = {"groups": []};
 server.listen(7331);
-/*
-# vim tabstop=8 expandtab shiftwidth=2 softtabstop=2
-*/
+// vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
