131e925e018f176ab7affc78fc6dd412675496e2
John Comeau <jc@unternet.net>
2017-06-21 15:46:41 -0700

    broken, no idea why.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var webpage = fs.readFileSync("index.html", "utf8");
 var app = function(request, response) {
+  console.log("app got request");
   var body = "";
   /* try to prevent malicious users from fetching files out of webspace
    * such as /etc/passwd by making everything relative to "here" and
@@ -9,24 +10,24 @@ var app = function(request, response) {
   var url = decodeURI(request.url)
     .replace(/^\/*/, "")
     .replace(/\/[.][.]\//g, "/");
-  console.log("got " + request.method + " for " + url);
+  console.log("got " + request.method + " for \"" + url + "\"");
   if (request.method == "GET") {
     fs.readFile(url, function(error, data) {
       if (error) data = webpage;
       response.write(data);
-      response.end();
     });
   } else if (request.method == "POST") {
     request.on("data", function(chunk) {body += chunk.toString();});
     request.on("end", function() {
       console.log("got POST: " + body);
+      console.log("got POST: " + JSON.stringify(getargs(body)));
       response.statusCode = 304;  // document not modified
-      response.end();
     });
   } else {
     console.error("unimplemented method: " + request.method);
     response.statusCode = 500;  // server error
   }
+  response.end();
 };
 var server = require("http").createServer(app);
 console.log("server: " + server);
