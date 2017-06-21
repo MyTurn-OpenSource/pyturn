c2640007ed528c8c8ca9b82a68962896b788d4e0
John Comeau <jc@unternet.net>
2017-06-21 16:21:37 -0700

    getargs doesn't yet work, but doesn't crash server

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ var app = function(request, response) {
     fs.readFile(url, function(error, data) {
       if (error) data = webpage;
       response.write(data);
+      response.end();
     });
   } else if (request.method == "POST") {
     request.on("data", function(chunk) {body += chunk.toString();});
@@ -22,16 +23,26 @@ var app = function(request, response) {
       console.log("got POST: " + body);
       console.log("got POST: " + JSON.stringify(getargs(body)));
       response.statusCode = 304;  // document not modified
+      response.end();
     });
   } else {
     console.error("unimplemented method: " + request.method);
     response.statusCode = 500;  // server error
+    response.end();
   }
-  response.end();
 };
 var server = require("http").createServer(app);
 console.log("server: " + server);
 var io = require("socket.io").listen(server);
 var data = {"groups": []};
+var getargs = function(string) {
+  var list = decodeURIComponent(string.replace("+", "%20")).split("&");
+  var args = {}, item, key, value;
+  for (var index = 0; index < list.length; index++) {
+    item = list[index].split("=", 1);
+    args[item[0]] = item[1];
+  }
+  return args;
+};
 server.listen(7331);
 // vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
