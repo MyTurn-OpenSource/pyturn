13a56fb017ec3e80b95b18399d6799e71806c0d6
John Comeau <jc@unternet.net>
2017-06-21 22:55:43 -0700

    client code not yet working correctly

--- client.js
+++ client.js
@@ -1,5 +1,23 @@
+// namespace this module.
+if (typeof(com) == "undefined") var com = {};
+if (typeof(com.jcomeau) == "undefined") com.jcomeau = {};
+com.jcomeau.myturn = {};
+com.jcomeau.myturn.join = function(click) {
+    click = click || window.event;  // for older MSIE versions
+    if ("bubbles" in click) click.stopPropagation();
+    else click.cancelBubble = true;  // MSIE < 9
+    var target = click.target || click.srcElement;  // also for MSIE
+    console.log("event: " + JSON.stringify(click));
+    console.log("target: " + JSON.stringify(target));
+};
 window.addEventListener("load", function() {
-	document.body.className = "";
-	loadindicator.style.display = "none";
-	wrapper.style.display = "table-cell";
+    var cjm = com.jcomeau.myturn;
+    document.body.className = "";
+    loadindicator.style.display = "none";
+    wrapper.style.display = "table-cell";
+    document.querySelector("input[name=Join]").addEventListener(
+        "click", cjm.join);
 });
+/*
+   vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+*/
