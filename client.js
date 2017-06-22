7faa21cfe6061e248f89c7b144b67209910374d9
John Comeau <jc@unternet.net>
2017-06-22 07:23:50 -0700

    clicking Join now trapped by client code

--- client.js
+++ client.js
@@ -3,20 +3,20 @@ if (typeof(com) == "undefined") var com = {};
 if (typeof(com.jcomeau) == "undefined") com.jcomeau = {};
 com.jcomeau.myturn = {};
 com.jcomeau.myturn.join = function(click) {
+    console.log("handling Join on client side");
     click = click || window.event;  // for older MSIE versions
-    if ("bubbles" in click) click.stopPropagation();
-    else click.cancelBubble = true;  // MSIE < 9
+    click.preventDefault();  // don't send formdata to server
+    console.log("click: " + JSON.stringify(click));
     var target = click.target || click.srcElement;  // also for MSIE
-    console.log("event: " + JSON.stringify(click));
     console.log("target: " + JSON.stringify(target));
+    return false;  // works even on very old browsers to prevent default action
 };
 window.addEventListener("load", function() {
     var cjm = com.jcomeau.myturn;
     document.body.className = "";
     loadindicator.style.display = "none";
     wrapper.style.display = "table-cell";
-    document.querySelector("input[name=Join]").addEventListener(
-        "click", cjm.join);
+    document.querySelector("input[value=Join]").onclick = cjm.join;
 });
 /*
    vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
