08740fba392145944ee5b936f81a5947375df508
John Comeau <jc@unternet.net>
2017-07-24 21:05:59 -0700

    got rid of form processing on client side for now eliminated top green bar by moving anchor into div and inside span

--- html/client.js
+++ html/client.js
@@ -2,36 +2,6 @@
 if (typeof(com) == "undefined") var com = {};
 if (typeof(com.jcomeau) == "undefined") com.jcomeau = {};
 com.jcomeau.myturn = {};
-com.jcomeau.myturn.join = function(click) {
-    console.log("handling Join on client side");
-    var cjm = com.jcomeau.myturn;
-    click = click || window.event;  // for older MSIE versions
-    var name = document.querySelector("input[name=name]");
-    var newGroupName = document.querySelector("input[name=newgroup]");
-    var groupName = document.querySelector("select[name=group]");
-    var selectGroup = document.querySelector("div[id=selectgroup]");
-    console.log("name: " + name.value +
-                ", newGroupName: " + newGroupName.value +
-                ", groupName: " + groupName.value);
-    if (name.value == "") return true;  // let the app fail, browser too old
-    if (click) click.preventDefault();
-    if (groupName.value == "") {
-        if (newGroupName.value == "") {
-            selectGroup.style.display = "none";
-            newgroup.style.display = "table-row";
-            newGroupName.setAttribute("required", "");
-        } else {
-            console.log("now populate the select with new group name");
-            var packet = {
-                name: name.value,
-                group: newGroupName.value
-            };
-            console.log("sending packet " + JSON.stringify(packet));
-            cjm.socket.emit("join", packet);
-        }
-    }
-    return false;  // works even on very old browsers to prevent default action
-};
 window.addEventListener("load", function() {
     var cjm = com.jcomeau.myturn;
     // leave the following 4 lines for last...
