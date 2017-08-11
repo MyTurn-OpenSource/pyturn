ae095570dd645c0ff3b4d5dfda52e2fe9777b78d
John Comeau <jc@unternet.net>
2017-08-11 00:20:16 -0700

    client.js:7 onload routine started 2017-08-11 00:19:24.807 client.js:9 location.path: undefined 2017-08-11 00:19:24.840 VM67:35 WebSocket connection to 'ws://myturn/socket.io' failed: Error during WebSocket handshake: Unexpected response code: 500

--- html/client.js
+++ html/client.js
@@ -2,11 +2,14 @@
 if (typeof(com) == "undefined") var com = {};
 if (typeof(com.jcomeau) == "undefined") com.jcomeau = {};
 com.jcomeau.myturn = {};
-window.addEventListener("load", function() {
+// no need to use `window.` anything, it is implied
+addEventListener("load", function() {
+    console.log("onload routine started");
     var cjm = com.jcomeau.myturn;
     console.log("location.path: " + location.path);
     if (location.path != "/noscript") {
         cjm.websocket = new WebSocket("ws:" + location.host + "/socket.io");
+        // save this redirect for last, only reached if all other tests pass
         if (location.path == "/") {
             location.replace(location.href + "app");
         }
