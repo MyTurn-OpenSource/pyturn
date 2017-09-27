2a64acdf04c0267ba3154f783fcbd0a3fa5f70c2
John Comeau <jc@unternet.net>
2017-09-26 19:02:17 -0700

    first stab at polling

--- html/client.js
+++ html/client.js
@@ -4,7 +4,19 @@ if (typeof(com.jcomeau) == "undefined") com.jcomeau = {};
 com.jcomeau.myturn = {};
 com.jcomeau.myturn.state = null;
 com.jcomeau.myturn.pages = null;
+com.jcomeau.myturn.page = null;
+com.jcomeau.myturn.storedPages = [];
+com.jcomeau.myturn.poller = null;
 // no need to use `window.` anything; it is implied
+com.jcomeau.myturn.poll = function() {
+    var request = XMLHttpRequest();  // not supporting IE
+    request.open("GET", "/ajax");
+    request.onreadystatechange = function() {
+        console.log("response code " + request.readyState + ": " +
+                    request.response + "(" + request.responseText + ")");
+    };
+    request.send();
+}
 addEventListener("load", function() {
     console.log("onload routine started");
     var cjm = com.jcomeau.myturn;
@@ -15,6 +27,17 @@ addEventListener("load", function() {
         cjm.state = "loading";
         cjm.pages = document.querySelectorAll("div.body");
         console.log("pages: " + cjm.pages);
+        for (var page of cjm.pages) {
+            if (page.style.display == "none") {
+                cjm.storedPages.push(page);
+                page.parentNode.removeChild(page);
+                // OK to set it visible now, it's no longer part of document
+                page.style.display = "";
+            } else {
+                cjm.page = page;
+            }
+        }
+        cjm.poller = setInterval(cjm.poll, 10000); // FIXME set to 500 (1/2 s)
         // save this redirect for last, only reached if all other tests pass
         if (path == "/") location.replace(location.href + "app");
         cjm.state = "loaded";
