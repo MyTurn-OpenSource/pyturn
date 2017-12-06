4633c42ea9d3a0b35b565909363e252c15a5ec07
John Comeau <jc@unternet.net>
2017-12-06 10:27:23 -0800

    bootstrapping multi-user and stress tests

--- myturn_test.py
+++ myturn_test.py
@@ -7,11 +7,70 @@ this one is geared to pyturn
 import sys, os, unittest, time, logging
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.action_chains import ActionChains
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 
 class TestMyturnApp(unittest.TestCase):
     '''
-    Various tests of app functionality
+    Various tests of basic app functionality
+    '''
+
+    def setUp(self):
+        '''
+        Initialize test environment
+        '''
+        capabilities = DesiredCapabilities.PHANTOMJS
+        capabilities['loggingPrefs'] = {'browser': 'ALL'}
+        self.driver = webdriver.PhantomJS(desired_capabilities=capabilities)
+
+    def test_load(self):
+        '''
+        Make sure JavaScript runs in headless browser
+        '''
+        self.driver.get('http://uwsgi-alpha.myturn.local')
+        time.sleep(1)  # enough time for redirect
+        logging.debug('current URL: %s', self.driver.current_url)
+        for entry in self.driver.get_log('browser'):
+            logging.debug('browser log entry: %s', entry)
+        self.assertTrue(self.driver.current_url.endswith('/app'))
+
+    def tearDown(self):
+        '''
+        Cleanup after testing complete
+        '''
+        self.driver.quit()
+
+class TestMyturnAppMultiUser(unittest.TestCase):
+    '''
+    Various tests of interaction between app and multiple users
+    '''
+
+    def setUp(self):
+        '''
+        Initialize test environment
+        '''
+        self.driver = webdriver.PhantomJS()
+
+    def test_load(self):
+        '''
+        Make sure JavaScript runs in headless browser
+        '''
+        self.driver.get('http://uwsgi-alpha.myturn.local')
+        time.sleep(1)  # enough time for redirect
+        logging.debug('current URL: %s', self.driver.current_url)
+        for entry in self.driver.get_log('browser'):
+            logging.debug('browser log entry: %s', entry)
+        self.assertTrue(self.driver.current_url.endswith('/app'))
+
+    def tearDown(self):
+        '''
+        Cleanup after testing complete
+        '''
+        self.driver.quit()
+
+class TestMyturnStress(unittest.TestCase):
+    '''
+    Stress-test server
     '''
 
     def setUp(self):
